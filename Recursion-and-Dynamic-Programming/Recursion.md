4ef34d577fc96d63546284354a3bc88639f16a1b
ar[arnau321 <arnau123@att.net>
2021-10-24 10:35:36 -0400

    Recursion study

---
+++ Recursion-and-Dynamic-Programming/Recursion.md
@@ -0,0 +1,13 @@
+### How to Approach
+- BOTTOM UP
+  - Solve for simple use case
+  - build the solution for one and keep adding additional solutions
+- TOP DOWN
+  - divide the problem into sub-problems
+  - make sure the sub-problems don't overlap
+- HALF-AND-HALF
+  - Divide the data set in half
+  - binary search and merge sort
+### Dynamic Programming and Memoization
+- DYNAMIC PROGRAMMING
+  - taking a recursive algorithm and finding the overlapping sub-problems(repeated calls).  Then you cache those results for future recursive calls.
---
+++ Recursion-and-Dynamic-Programming/compute-nth-fib.js
@@ -0,0 +1,52 @@
+// find the nth Fibonacci number
+
+// function fibonacci(i) {
+//   if(i === 0) return i
+//   if(i === 1) return i
+//   return fibonacci(i - 1) + fibonacci(i - 2)
+// }
+
+// with memoiztion
+let fibonacci= (function() {
+  // creates the memo to store replicated values
+  let memo = {}
+
+  function f(n) {
+    let value
+    // if n is in memo set value to memo
+    if(n in memo) {
+      value = memo[n]
+    // else calculate the fib value
+    } else {
+      if(n===0||n===1)
+        value = n
+        else
+          value = f(n-1) + f(n-2)
+        
+        memo[n] = value  
+    }
+    return value
+  }
+  return f
+
+})()
+
+// a bottom up approach
+function fib(n){
+  // base cases
+  if(n === 0 || n === 1) return n
+
+  // create memo
+  let memo = []
+  // set base memos
+  memo[0] = 0
+  memo[1] = 1
+  for(let i = 2; i < n; i++){
+    memo[i] = memo[i - 1] + memo[i - 2]
+  }
+  return memo[n-1] + memo[n-2]
+}
+
+
+console.log(fibonacci(1))
+console.log(fib(5))
