59e3ec65ce1b0797831fe3f53dbb6bd6715fa7dc
ar[arnau321 <arnau123@att.net>
2022-01-16 09:50:06 -0500

    Add notes

--- leet/Arrays/Javascript/two-sum.js
+++ leet/Arrays/Javascript/two-sum.js
@@ -9,11 +9,18 @@
 // Objective: return indices of two numbers in array that add to target
 // can't use the same element twice, numbers not unique
 
+// brute force algorithm
 function twoSum(nums, target) {
+  // return answer array
   let an = []
+  // move through elements in array
   for(let i = 0; i < nums.length; i++) {
+    // set first number to add to get to target
     let num = nums[i]
+    // move through elements in array
     for(let j = 0; j < nums.length; j++) {
+      // check if current element + num is = target
+      // if = target push to answer array and return answer
       if(i !== j && num + nums[j] === target){
         an.push(i)
         an.push(j)
@@ -26,22 +33,23 @@ function twoSum(nums, target) {
 // uses hash table
 function twoSum2(nums, target) {
   let numbObject = {}         // hash table
-
-  // fill hash table
+  
+  // hash function to create hash table of objects key:value
   for(let i = 0; i < nums.length; i++){
+    // sets key for hash table
     let num = nums[i]
-    // sets key to number and index to value '5': 1
+    // sets i as return value for key from hash table
     numbObject[num] = i
   }
+  
 
+  // iterate through nums length
   for(let i = 0; i < nums.length; i++){
     // gets a target number to search for
     let diff = target - nums[i]
 
     // if hash table has number and the value
     // and makes sure not using the same element twice
-    console.log(numbObject)
-    console.log(diff, numbObject[diff], i)
     if(numbObject.hasOwnProperty(diff) && numbObject[diff] !== i) {
       // return index of numbers used in any order
       return [i, numbObject[diff]]
