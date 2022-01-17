c87220b4d01776757c408aa8aec96c38677fb73d
ar[arnau321 <arnau123@att.net>
2022-01-16 19:12:48 -0500

    Add notes

--- leet/Arrays/Javascript/find-min -in-rotated-sorted-array.js
+++ leet/Arrays/Javascript/find-min -in-rotated-sorted-array.js
@@ -8,21 +8,28 @@
 
 // Objective: return an sorted array from min to max by "rotating"
 // given array
-// hint: use binary search
+// hint: use binary search and recursion
 
 function findMin(nums) {
+  // for outlier condition
+  if (nums.length === 1) return nums
 
-  if(nums.length === 1) return nums
-
-  // find midpoint
+  // find middle index of array
   const midIndex = Math.floor(nums.length / 2)
-  console.log('mid ', midIndex)
+  
+  // if value at center of array is less than value
+  // to the left of the center return middle index value
+  // ends recursion
   if(nums[midIndex] < nums[midIndex - 1]) {
     return nums[midIndex]
-  } else if (nums[midIndex] > nums[nums.length - 1]) {
-    
+  // if value at center of array is greater than value
+  // to the left of the center recurse array using values
+  // to the right of the middle index
+  } else if (nums[midIndex] > nums[nums.length - 1]) { 
     return findMin(nums.slice(midIndex + 1))
   }
+  // recursive call to find min using the values
+  // to the left of middle index value an include middle index value
   return findMin(nums.slice(0, midIndex))
 }
 
