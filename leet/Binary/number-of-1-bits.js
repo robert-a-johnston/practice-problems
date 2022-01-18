dcd6a82854beff518d9485e0cef9cbf68284eb6b
ar[arnau321 <arnau123@att.net>
2022-01-18 12:40:09 -0500

    Add notes

--- leet/Binary/number-of-1-bits.js
+++ leet/Binary/number-of-1-bits.js
@@ -12,13 +12,18 @@
 
 // Objective: 
 function hammingWeight(n) {
-  var sum  = 0
+  // Return number of 1 bits
+  let numberOfOneBits  = 0
 
-  var arr = n.toString(2).split('').map(el => {
-      el === "1" ?  sum++ : sum +=0
+  // Convert n to string and split into array of elements 0s and 1s
+  // 2 in toString(2) indicates base 2 or binary representation
+  const arr = n.toString(2).split('')
+  // Iterate over each element and add 1 to count of number of 1s
+  arr.map(element => {
+      element === "1" ?  numberOfOneBits++ : numberOfOneBits+=0
   })
-  return sum
+  return numberOfOneBits
 
 }
 let n = 011011
-console.log(hammingWeight(n))
+console.log(hammingWeight(n)) // 4
